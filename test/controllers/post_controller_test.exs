2e4256283c098b2e39ccd41cd96f837468b757ba
enzomalonzo <enzo_malonzo27@yahoo.com>
2017-02-17 19:37:27 +0800

    Adds role (admin) and association of TestHelper

--- test/controllers/post_controller_test.exs
+++ test/controllers/post_controller_test.exs
@@ -1,35 +1,25 @@
 defmodule Pxblog.PostControllerTest do
   use Pxblog.ConnCase
 
-  alias Pxblog.Post
-  @valid_attrs %{body: "some content", title: "some content"}
-  @invalid_attrs %{body: "", title: ""}
+  alias Pxblog.{
+    Post,
+    TestHelper
+  }
 
-  alias Pxblog.User
+  @valid_attrs %{body: "some content", title: "some content"}
+  @invalid_attrs %{}
 
   setup do
-    {:ok, user} = create_user
-      conn = build_conn()
-      |> login_user(user)
-    {:ok, conn: conn, user: user}
-  end
+    role  = TestHelper.create_role(%{name: "User Role", admin: false})
+    user  = TestHelper.create_user(role, %{email: "test@test.com", username: "testuser", password: "test", password_confirmation: "test"})
+    post  = TestHelper.create_post(user, %{title: "Test Post", body: "Test Body"})
 
-  defp create_user do
-    %User{}
-    |> User.changeset(%{email: "test@test.com", username: "test", password: "test", password_confirmation: "test"})
-    |> Repo.insert
+    conn = build_conn() |> login_user(user)
+    {:ok, conn: conn, user: user, role: role, post: post}
   end
 
   defp login_user(conn, user) do
     post conn, session_path(conn, :create), user: %{username: user.username, password: user.password}
-  end
-
-  defp build_post(user) do
-    changeset =
-      user
-      |> build_assoc(:posts)
-      |> Post.changeset(@valid_attrs)
-      |> Repo.insert!(changeset)
   end
 
   test "lists all entries on index", %{conn: conn, user: user} do
@@ -57,8 +47,7 @@ defmodule Pxblog.PostControllerTest do
     assert html_response(conn, 200) =~ "New post"
   end
 
-  test "shows chosen resource", %{conn: conn, user: user} do
-    post = build_post(user)
+  test "shows chosen resource", %{conn: conn, user: user, post: post} do
     conn = get conn, user_post_path(conn, :show, user, post)
 
     assert html_response(conn, 200) =~ "Show post"
@@ -70,30 +59,26 @@ defmodule Pxblog.PostControllerTest do
     end
   end
 
-  test "renders form for editing chosen resource", %{conn: conn, user: user} do
-    post = build_post(user)
+  test "renders form for editing chosen resource", %{conn: conn, user: user, post: post} do
     conn = get conn, user_post_path(conn, :edit, user, post)
 
     assert html_response(conn, 200) =~ "Edit post"
   end
 
-  test "updates chosen resource and redirects when data is valid", %{conn: conn, user: user} do
-    post = build_post(user)
+  test "updates chosen resource and redirects when data is valid", %{conn: conn, user: user, post: post} do
     conn = put conn, user_post_path(conn, :update, user, post), post: @valid_attrs
 
     assert redirected_to(conn) == user_post_path(conn, :show, user, post)
     assert Repo.get_by(Post, @valid_attrs)
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, user: user} do
-    post = build_post(user)
-    conn = put conn, user_post_path(conn, :update, user, post), post: @invalid_attrs
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, user: user, post: post} do
+    conn = put conn, user_post_path(conn, :update, user, post), post: %{"body" => nil}
 
     assert html_response(conn, 200) =~ "Edit post"
   end
 
-  test "deletes chosen resource", %{conn: conn, user: user} do
-    post = build_post(user)
+  test "deletes chosen resource", %{conn: conn, user: user, post: post} do
     conn = delete conn, user_post_path(conn, :delete, user, post)
 
     assert redirected_to(conn) == user_post_path(conn, :index, user)
@@ -109,10 +94,8 @@ defmodule Pxblog.PostControllerTest do
     #assert conn.halted
   #end
 
-  test "redirects when trying to edit a post for a different user", %{conn: conn, user: user} do
-    other_user = User.changeset(%User{}, %{email: "test2@test.com", username: "test2", password: "test", password_confirmation: "test"})
-      |> Repo.insert!
-    post = build_post(user)
+  test "redirects when trying to edit a post for a different user", %{conn: conn, role: role, post: post} do
+    other_user = TestHelper.create_user(role, %{email: "test2@test.com", username: "test2", password: "test", password_confirmation: "test"})
     conn = get conn, user_post_path(conn, :edit, other_user, post)
 
     assert get_flash(conn, :error) == "You are not authorized to modify that post!"
--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,24 +1,27 @@
 defmodule Pxblog.LayoutViewTest do
   use Pxblog.ConnCase, async: true
 
-  alias Pxblog.LayoutView
-  alias Pxblog.User
+  alias Pxblog.{
+    LayoutView,
+    TestHelper
+  }
 
   setup do
-    %User{}
-    |> User.changeset(%{username: "test", password: "test", password_confirmation: "test", email: "test@test.com"})
-    |> Repo.insert!
-    {:ok, conn: build_conn()}
+    role = TestHelper.create_role(%{name: "User Role", admin: false})
+    user = TestHelper.create_user(role, %{email: "test@test.com", username: "testuser", password: "test", password_confirmation: "test"})
+
+    {:ok, conn: build_conn(), user: user}
   end
 
-  test "current user returns the user in the session", %{conn: conn} do
-    conn = post conn, session_path(conn, :create), user: %{username: "test", password: "test"}
+  test "current user returns the user in the session", %{conn: conn, user: user} do
+    conn = post conn, session_path(conn, :create), user: %{username: user.username, password: user.password}
+
     assert LayoutView.current_user(conn)
   end
 
-  test "current user returns nothing if there is no user in the session", %{conn: conn} do
-    user = Repo.get_by(User, %{username: "test"})
+  test "current user returns nothing if there is no user in the session", %{conn: conn, user: user} do
     conn = delete conn, session_path(conn, :delete, user)
+
     refute LayoutView.current_user(conn)
   end
 end
